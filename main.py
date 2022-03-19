b5bc1759308710eacd76c976f92454f3208dffd8
yoav-shamay <yoav.s@bengurion.org>
2022-03-19 22:14:34 +0200

    started adding db

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
 import flask
 from flask import request
+from flask_sqlalchemy import SQLAlchemy #import sqlalchemy
 
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db' #set the config to use the database (which I called database.db)
+db = SQLAlchemy(app) #create an SQLAlchemy instance
+
 @app.route('/search', methods=['GET'])
 def search():
-    resp = flask.Response(request.args.get('a') + request.args.get('b'))
+    #lets say this function will search by name (just to test the db)
+    name = request.args.get("name")
+    st = Student.query.filter_by(name=name).all() #get all students with the given name, this is the syntax
+    resp = flask.Response(str(st))
     resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
 
 @app.route('/register', methods=['GET'])
 def register():
+    name = request.args.get("name")
+    age = request.args.get("age")
+    st = Student(name=name, age=age) #no need to create an id, sqlite does it to us automatically
+    db.session.add(st) #add the student to the database (will automatically add it to specifically the student table)
+    db.session.commit() #commit the changes, needed after every change to actually save it in the database
     return "success"
+
+if __name__ == "__main__":
+    from student import Student #import the student class from the student file (since the student file also imports the db from this file, put the import later to avoid errors)
+    db.create_all() #automatically create all the tables, since we configured the student class as a table, it will automatically create it if neede
+    app.run()
+#install the flask-sqlalchemy addon in File->Settings->Project: bengurionserver->Project Interpreter->+
+#I already installed sqlite and everything needed under the localDatabase folder
+#I already created a database called database.db
+#db table is made out of rows and columns - each row is a student, while each column is its property. right now I created id, name and age (each table needs an id column)
--- student.py
+++ student.py
@@ -1,9 +1,14 @@
-class Student:
-   def __init__(self,name,age):
-       self._name=name
-       self._age=age
-   def display(self):
-       print(self._name,self._age)
+from main import db #import the db from main.py in order to use it here
+
+class Student(db.Model): #inherit from db.Model to connect to the database
+    id = db.Column('id', db.Integer, primary_key=True, autoincrement=True) #create an id column. each row in the database needs unique id, mark its as primary key since this is the id the database will use
+    name = db.Column(db.String(20), nullable=False) #mark the name as column of string with maximum 20 characters. nullable=false means it cant be null, it must be filled
+    age = db.Column(db.Integer, nullable=False) #mark the age as an integer column
+    def display(self):
+        print(self.name,self.age)
+
+'''
+Comment this to avoid running this (because we import this file)
 stu1=""
 stu2=""
 stu3=""
@@ -14,5 +19,5 @@ for i in student:
   i= Student(input("enther your name:"),input("enter your age:"))
 
   i.display()
-
+'''
 
